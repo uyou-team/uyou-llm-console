6cfcf6dbdc02631bb257a8ad24c983b373854eba
Anthony Lu <58923223+tonylu110@users.noreply.github.com>
2025-05-07 13:29:33 +0800

    feat: set system prompt

--- index.ts
+++ index.ts
@@ -11,13 +11,15 @@ const rl = readline.createInterface({
 })
 
 function choose() {
-    rl.question('Choose: \n1: set Model\n2: set Api\n3: set System Promat\n4: To chat\n', async (answer) => {
+    rl.question('Choose: \n1: set Model\n2: set Api\n3: set System Prompt\n4: To chat\n', async (answer) => {
         if (answer === '1') {
             setModel()
         } else if (answer === '2') {
             setApi()
         } else if (answer === '3') {
-
+            setSystemPrompt()
+        } else if (answer === '/exit'.toLowerCase()) {
+            rl.close()
         } else {
             const config = JSON.parse(await fs.promises.readFile(path.resolve(__dirname, 'config.json'), 'utf-8'))
             const models = await (await ollama.list()).models
@@ -79,9 +81,35 @@ async function setModel() {
     })
 }
 
+function setSystemPrompt() {
+    rl.question('Please enter your system prompt:\n', (answer) => {
+        fs.readFile(path.resolve(__dirname, 'config.json'), 'utf-8', (err, data) => {
+            if (err) {
+                console.error('Error reading file:', err)
+                return
+            }
+            const config = JSON.parse(data)
+            config.systemPrompt = answer.trim()
+            fs.writeFile(path.resolve(__dirname, 'config.json'), JSON.stringify(config), (err) => {
+                if (err) {
+                    console.error('Error writing to file:', err)
+                    return
+                }
+                console.log('System prompt saved successfully.')
+                choose()
+            })
+        })
+    })
+}
+
 async function chat() {
     const config = JSON.parse(await fs.promises.readFile(path.resolve(__dirname, 'config.json'), 'utf-8'))
     const models = await (await ollama.list()).models
+
+    if (config.systemPrompt) {
+        const systemPrompt = config.systemPrompt
+        console.log(`System: ${systemPrompt}`);   
+    }
 
     rl.question('You: ', async (input) => {
         if (input.toLowerCase() === '/exit') {
@@ -94,7 +122,7 @@ async function chat() {
         }
         const response = await ollama.chat({
             model: config.model ? config.model : models[0].model,
-            messages: [{ role: 'user', content: input }],
+            messages: [{role: 'system', content: config.systemPrompt}, { role: 'user', content: input }],
             stream: true
         })
         console.log('Bot:')
