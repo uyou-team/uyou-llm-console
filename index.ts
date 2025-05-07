5529178e0d1527de4adf81dcb2971b5c9b9d84eb
Anthony Lu <58923223+tonylu110@users.noreply.github.com>
2025-05-08 07:41:49 +0800

    feat: set automatically enter chat on startup

--- index.ts
+++ index.ts
@@ -11,7 +11,7 @@ const rl = readline.createInterface({
 })
 
 function choose() {
-    rl.question('Choose: \n1: set Model\n2: set Api\n3: set System Prompt\n4: To chat\n', async (answer) => {
+    rl.question('Choose: \n1: set Model\n2: set Api\n3: set System Prompt\n4: To chat\n5: settings\n', async (answer) => {
         if (answer === '1') {
             setModel()
         } else if (answer === '2') {
@@ -20,11 +20,18 @@ function choose() {
             setSystemPrompt()
         } else if (answer === '/exit'.toLowerCase()) {
             rl.close()
-        } else {
+        } else if (answer === '4') {
             const config = JSON.parse(await fs.promises.readFile(path.resolve(__dirname, 'config.json'), 'utf-8'))
             const models = await (await ollama.list()).models
-            console.log(`Start to chat (model: ${config.model ? config.model : models[0].model})`)
+            console.log(`\nStart to chat (model: ${config.model ? config.model : models[0].model})`)
+            
+            if (config.systemPrompt) {
+                const systemPrompt = config.systemPrompt
+                console.log(`\nSystem: ${systemPrompt}`);   
+            }
             chat()
+        } else {
+            settings()
         }
     })
 }
@@ -108,11 +115,6 @@ async function chat() {
     const config = JSON.parse(await fs.promises.readFile(path.resolve(__dirname, 'config.json'), 'utf-8'))
     const models = await (await ollama.list()).models
 
-    if (config.systemPrompt) {
-        const systemPrompt = config.systemPrompt
-        console.log(`System: ${systemPrompt}`);   
-    }
-
     rl.question('You: ', async (input) => {
         if (input.toLowerCase() === '/exit') {
             rl.close()
@@ -142,14 +144,46 @@ async function chat() {
     })
 }
 
+async function settings() {
+    const config = JSON.parse(await fs.promises.readFile(path.resolve(__dirname, 'config.json'), 'utf-8'))
+
+    rl.question(`uyou llm console settings:\n1: set automatically enter chat on startup (${config.autoInChat ? config.autoInChat : 'off'})\n`, (answer) => {
+        if (answer === '1') {
+            const autoInChat = config.autoInChat === 'on' ? 'off' : 'on'
+            config.autoInChat = autoInChat
+            fs.writeFile(path.resolve(__dirname, 'config.json'), JSON.stringify(config), (err) => {
+                if (err) {
+                    console.error('Error writing to file:', err)
+                    return
+                }
+                console.log(`Auto enter chat on startup is now ${autoInChat}.`)
+                choose()
+            })
+        }
+    })
+}
+
 function init() {
-    fs.readFile(path.resolve(__dirname, 'config.json'), 'utf-8', (err, _data) => {
+    fs.readFile(path.resolve(__dirname, 'config.json'), 'utf-8', async (err, _data) => {
         if (err) {
             setApi()
             return
         }
         const config = fs.readFileSync(path.resolve(__dirname, 'config.json'), 'utf-8')
         ollama = new Ollama({ host: JSON.parse(config).apiLink })
+
+        if (JSON.parse(config).autoInChat === 'on') {
+            const config = JSON.parse(await fs.promises.readFile(path.resolve(__dirname, 'config.json'), 'utf-8'))
+            const models = await (await ollama.list()).models
+            console.log(`Start to chat (model: ${config.model ? config.model : models[0].model})`)
+            
+            if (config.systemPrompt) {
+                const systemPrompt = config.systemPrompt
+                console.log(`\nSystem: ${systemPrompt}`);   
+            }
+            chat()
+            return
+        }
     
         choose()
     })
